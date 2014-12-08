6fd02a7b319afac2d2ca4cff129298713780aa0b
Parth Brahmbhatt <brahmbhatt.parth@gmail.com>
2014-12-08 15:10:13 -0800

    Reverting back to TopicOffsetOutOfRangeException extends RunTimeException.

--- external/storm-kafka/src/jvm/storm/kafka/TopicOffsetOutOfRangeException.java
+++ external/storm-kafka/src/jvm/storm/kafka/TopicOffsetOutOfRangeException.java
@@ -1,6 +1,6 @@
 package storm.kafka;
 
-public class TopicOffsetOutOfRangeException extends FailedFetchException {
+public class TopicOffsetOutOfRangeException extends RuntimeException {
 
     public TopicOffsetOutOfRangeException(String message) {
         super(message);
