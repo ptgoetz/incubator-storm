apache/storm

STORM-2551: Add socket timeout for Thrift client

storm-client/src/jvm/org/apache/storm/security/auth/ThriftClient.java
@@ -56,6 +56,10 @@ public class ThriftClient implements AutoCloseable {
             port = type.getPort(topoConf);
         }
 
+        if (timeout == null) {
+            timeout = type.getSocketTimeOut(topoConf);
+        }
+
         if (port<=0 && !type.isFake()) {
             throw new IllegalArgumentException("invalid port: "+port);
         }          

ptgoetz/incubator-storm

storm-core/src/jvm/org/apache/storm/security/auth/ThriftClient.java
@@ -56,6 +56,10 @@ public class ThriftClient implements AutoCloseable {
             port = type.getPort(storm_conf);
         }
 
+        if (timeout == null) {
+            timeout = type.getSocketTimeOut(storm_conf);
+        }
+
         if (port<=0) {
             throw new IllegalArgumentException("invalid port: "+port);
         }          