apache/storm

[STORM-2191] shorten classpaths by using wildcards
Instead of fully enumerating all JARs in the lib directories, we just use
a Java classpath wildcard to allow the JVM to autodiscover all JARs in the
classpath.  This affects both the Worker and LogWriter processes, as well
as Storm daemons such as the Nimbus, UI, Logviewer, and Supervisor.

This change results in shorter commands, so that you can actually see
the full content of the Worker command in `ps` output on Linux.

Prior to this change Worker commands were easily longer than 4096 bytes,
which is the default Linux kernel limit for commands being recorded into
the process table.  Longer commands get truncated, though they do get
executed.

An example of the change in Worker classpath length can be seen here:

Before:
```
-cp STORM_DIR/lib-worker/asm-5.0.3.jar:STORM_DIR/lib-worker/carbonite-1.5.0.jar:STORM_DIR/lib-worker/chill-java-0.8.0.jar:STORM_DIR/lib-worker/clojure-1.7.0.jar:STORM_DIR/lib-worker/commons-codec-1.6.jar:STORM_DIR/lib-worker/commons-collections-3.2.2.jar:STORM_DIR/lib-worker/commons-io-2.5.jar:STORM_DIR/lib-worker/commons-lang-2.5.jar:STORM_DIR/lib-worker/commons-logging-1.1.3.jar:STORM_DIR/lib-worker/curator-client-2.12.0.jar:STORM_DIR/lib-worker/curator-framework-2.12.0.jar:STORM_DIR/lib-worker/disruptor-3.3.2.jar:STORM_DIR/lib-worker/guava-16.0.1.jar:STORM_DIR/lib-worker/httpclient-4.3.3.jar:STORM_DIR/lib-worker/httpcore-4.4.1.jar:STORM_DIR/lib-worker/jgrapht-core-0.9.0.jar:STORM_DIR/lib-worker/jline-0.9.94.jar:STORM_DIR/lib-worker/json-simple-1.1.jar:STORM_DIR/lib-worker/kryo-3.0.3.jar:STORM_DIR/lib-worker/kryo-shaded-3.0.3.jar:STORM_DIR/lib-worker/libthrift-0.9.3.jar:STORM_DIR/lib-worker/log4j-api-2.8.jar:STORM_DIR/lib-worker/log4j-core-2.8.jar:STORM_DIR/lib-worker/log4j-over-slf4j-1.6.6.jar:STORM_DIR/lib-worker/log4j-slf4j-impl-2.8.jar:STORM_DIR/lib-worker/minlog-1.3.0.jar:STORM_DIR/lib-worker/netty-3.9.0.Final.jar:STORM_DIR/lib-worker/objenesis-2.1.jar:STORM_DIR/lib-worker/reflectasm-1.

storm-server/src/main/java/org/apache/storm/daemon/supervisor/BasicContainer.java
@@ -22,6 +22,7 @@ import static org.apache.storm.utils.Utils.OR;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -334,19 +335,13 @@ public class BasicContainer extends Container {
     }
 
     /**
-     * Returns a collection of jar file names found under the given directory.
-     * @param dir the directory to search
-     * @return the jar file names
+     * Returns a path with a wildcard as the final element, so that the JVM will expand
+     * that to all JARs in the directory.
+     * @param dir the directory to which a wildcard will be appended
+     * @return the path with wildcard ("*") suffix
      */
-    protected List<String> getFullJars(File dir) {
-        File[] files = dir.listFiles(jarFilter);
-
-        if (files == null) {
-            return Collections.emptyList();
-        }
-
-        return Arrays.stream(files).map(f -> f.getAbsolutePath())
-                .collect(Collectors.toList());
+    protected String getWildcardDir(File dir) {
+        return Paths.get(dir.toString(), "*").toString();
     }
     
     protected List<String> frameworkClasspath() {
@@ -358,8 +353,8 @@ public class BasicContainer extends Container {
         File stormExtlibDir = new File(_stormHome, "extlib");
         String extcp = System.getenv("STORM_EXT_CLASSPATH");
         List<String> pathElements = new LinkedList<>();
-        pathElements.addAll(getFullJars(stormWorkerLibDir));
-        pathElements.addAll(getFullJars(stormExtlibDir));
+        pathElements.add(getWildcardDir(stormWorkerLibDir));
+        pathElements.add(getWildcardDir(stormExtlibDir));
         pathElements.add(extcp);
         pathElements.add(stormConfDir);
 

ptgoetz/incubator-storm

This commit resolves cherry-pick conflicts for backporting change from storm master branch.
Related commit in master: 2ceef0dce21895c4ef0f0b8eb5bc8248fad25e13

storm-core/src/jvm/org/apache/storm/daemon/supervisor/BasicContainer.java
@@ -20,6 +20,7 @@ package org.apache.storm.daemon.supervisor;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -329,21 +330,13 @@ public class BasicContainer extends Container {
     }
 
     /**
-     * Returns a collection of jar file names found under the given directory.
-     * @param dir the directory to search
-     * @return the jar file names
+     * Returns a path with a wildcard as the final element, so that the JVM will expand
+     * that to all JARs in the directory.
+     * @param dir the directory to which a wildcard will be appended
+     * @return the path with wildcard ("*") suffix
      */
-    protected List<String> getFullJars(File dir) {
-        File[] files = dir.listFiles(jarFilter);
-
-        if (files == null) {
-            return Collections.emptyList();
-        }
-        ArrayList<String> ret = new ArrayList<>(files.length);
-        for (File f: files) {
-            ret.add(f.getAbsolutePath());
-        }
-        return ret;
+    protected String getWildcardDir(File dir) {
+        return Paths.get(dir.toString(), "*").toString();
     }
     
     protected List<String> frameworkClasspath() {
@@ -355,8 +348,8 @@ public class BasicContainer extends Container {
         File stormExtlibDir = new File(_stormHome, "extlib");
         String extcp = System.getenv("STORM_EXT_CLASSPATH");
         List<String> pathElements = new LinkedList<>();
-        pathElements.addAll(getFullJars(stormLibDir));
-        pathElements.addAll(getFullJars(stormExtlibDir));
+        pathElements.add(getWildcardDir(stormLibDir));
+        pathElements.add(getWildcardDir(stormExtlibDir));
         pathElements.add(extcp);
         pathElements.add(stormConfDir);
 